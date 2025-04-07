d79cddb51543e98bc2a6a84a8f86540593743e14
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-04-07 20:36:25 +0530

    server setup with postgres

---
+++ server/src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./config/db.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+
+// Error handling
+
+// Test
+app.get("/", async(req, res) => {
+    const result = await pool.query("SELECT current_database()");
+    res.send(`The database name is : ${result.rows[0].current_database}`)
+})
+
+// Server running
+app.listen(port, () => console.log(`Server started on : http://localhost:${port}`))
