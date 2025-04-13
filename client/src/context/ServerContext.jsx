177b60950e044d629a2616f74cd4899a92ac472f
Reon23 <166202651+Reon23@users.noreply.github.com>
2025-04-13 20:28:26 +0530

    Server Context

---
+++ client/src/context/ServerContext.jsx
@@ -0,0 +1,69 @@
+import React, { createContext, useState } from "react";
+
+import axios from "axios";
+
+export const ServerContext = createContext();
+
+export const ServerProvider = ({ children }) => {
+
+    const [account, setAccount] = useState([]);
+
+    // Register methods
+    const registerInstructor = async (role, name, email, password) => {
+        instructor = {
+            "role": role,
+            "name": name,
+            "email": email,
+            "password": password
+        }
+        axios.post('http://localhost:5001/api/auth/register', instructor)
+        .then(res => {
+            console.log(res.data);
+            setAccount(res.data);
+        })
+        .catch(err => console.error(err));
+    }
+
+    const registerStudent = async (role, firstName, lastName, email, password) => {
+        student = {
+            "role": role,
+            "first_name": firstName,
+            "last_name": lastName,
+            "email": email,
+            "password": password
+        }
+        axios.post('http://localhost:5001/api/auth/register', student)
+        .then(res => {
+            console.log(res.data);
+            setAccount(res.data);
+        })
+        .catch(err => console.error(err));
+    }
+
+    // Login method
+    const loginUser = async (role, email, password) => {
+        user = {
+            "role": role,
+            "email": email,
+            "password": password
+        }
+        axios.post('http://localhost:5001/api/auth/login', user)
+        .then(res => {
+            console.log(res.data);
+            setAccount(res.data);
+        })
+        .catch(err => console.error(err));
+    }
+
+    return (
+        <ServerContext.Provider value={
+            {
+                account,
+                registerInstructor,
+                registerStudent,
+                loginUser,
+            }}>
+            {children}
+        </ServerContext.Provider>
+    )
+}
